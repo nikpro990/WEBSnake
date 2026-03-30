36049e6fe89291b0731fd802c1e0b7f0ddcd9013
17 <ai9622398@gmail.com>
2026-03-30 18:56:54 +0300

    new

--- scripts/script_index.js
+++ scripts/script_index.js
@@ -1,9 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const buttonLogin = document.getElementById("registration_js");
     const buttonStart = document.getElementById("start_js");
+    const buttonStatistics = document.getElementById("apple_count_js");
     const buttonExit = document.getElementById("exit_js");
+
+    buttonLogin.addEventListener("click", () => {
+
+    });
 
     buttonStart.addEventListener("click", () => {
         window.location.href = '/game/game.html';
+    });
+
+    buttonStatistics.addEventListener("click", () => {
+        window.location.href = '/counter_apple/counter_apple.html'
     });
 
     buttonExit.addEventListener("click", () => {
