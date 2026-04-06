a8d7af5ecf5152cb56d39576d228696c06e4f3d6
17 <ai9622398@gmail.com>
2026-04-06 18:56:32 +0300

    new

--- scripts/script_form.js
+++ scripts/script_form.js
@@ -1,21 +1,36 @@
-const ButtonForm = document.getElementById("form_js");
-const InputPassword = document.getElementById("password");
-const InputPasswordRepeat = document.getElementById("password_repeat")
-const buttonSumbit = document.getElementById("sumbit");
+document.addEventListener("DOMContentLoaded", () => {
+  const ButtonForm = document.getElementById("form_js");
+  let InputPassword = document.getElementById("password_js");
+  let InputPasswordRepeat = document.getElementById("password_repeat_js")
+  const buttonSumbit = document.getElementById("sumbit");
+  
+  buttonSumbit.disabled = true;
 
+  function checkValue(){  
+     const p1 = InputPassword.value;
+     const p2 = InputPasswordRepeat.value;
 
+     buttonSumbit.disabled = (p1 === "" || p2 === "" || p1 !== p2);
+  }
 
-ButtonForm.onsubmit = async (e) => {
-    e.preventDefault();
-    const data = {
-        username : document.getElementById("name").value,
-        password : document.getElementById("password").value, 
-    }
-    const response = await fetch("http://127.0.0.1:5500/form/form.html", {
-      method: "POST",
-      headers: {"Content-Type": "aplication/json"},
-      body: JSON.stringify(data)
-    });
-    const result = await response.text();
-    alert(result);
-};
+  InputPasswordRepeat.oninput = checkValue;
+  InputPassword.oninput = checkValue;
+
+  ButtonForm.onsubmit = async (e) => {
+      e.preventDefault();
+      const data = {
+          username : document.getElementById("name").value,
+          password : document.getElementById("password_js").value, 
+      }
+      const response = await fetch("http://127.0.0.1:5500/form/form.html", {
+        method: "POST",
+        headers: {"Content-Type": "aplication/json"},
+        body: JSON.stringify(data)
+      });
+
+      if(response.ok){
+        alert(result)
+      }
+      const result = await response.text();
+  };
+});
